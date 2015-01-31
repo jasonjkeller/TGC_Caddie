12f8112edfc767953bc556bf32a7e43cde5f0b11
Jason Keller <jasonjkeller@gmail.com>
2015-01-31 02:53:18 -0800

    Added ShotAnalyzer class

---
+++ src/ShotAnalyzer.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jasonjkeller on 1/31/15.
+ */
+public class ShotAnalyzer {
+    
+    // need to move logic from TgcCaddie into here
+    
+    public ShotAnalyzer() {
+        
+    }
+    
+    public double analyzeShot() {
+        return 1.0;
+    }
+
+    /** test **/
+    public static void main(String[] args) {
+
+    }
+}
